09469fd2ceace5f3979fff556fe978bd50767587
4strodev <jumarurex@gmail.com>
2023-05-11 16:15:29 +0200

    feat:remove node modules and refactor get data from api

--- js.js
+++ js.js
@@ -1,64 +1,66 @@
+async function getPersonatje(characterId) {
+    const response = await fetch(`https://rickandmortyapi.com/api/character/${characterId}`)
+    return await response.json();
+}
+
+async function getLocalitzacio(locationId) {
+    const response = await fetch(`https://rickandmortyapi.com/api/location/${locationId}`)
+    return await response.json();
+}
+
 // Funció per gestionar el clic al botó "Personatges"
-function handlePersonatgesClick() {
+async function handlePersonatgesClick() {
     const characterId = prompt("Introdueix l'ID del personatge:");
-  
+
     if (characterId !== null && characterId.trim() !== "") {
-      fetch(`https://rickandmortyapi.com/api/character/${characterId}`)
-        .then(response => response.json())
-        .then(data => {
-          const characterName = document.getElementById("character-name");
-          const characterStatus = document.getElementById("character-status");
-          const characterSpecies = document.getElementById("character-species");
-          const characterImage = document.getElementById("character-image");
-  
-          characterName.textContent = data.name;
-          characterStatus.textContent = data.status;
-          characterSpecies.textContent = data.species;
-          characterImage.innerHTML = `<img src="${data.image}" alt="${data.name}">`;
-        })
-        .catch(error => console.log(error));
+        const character = await getPersonatje(characterId)
+        const characterName = document.getElementById("character-name");
+        const characterStatus = document.getElementById("character-status");
+        const characterSpecies = document.getElementById("character-species");
+        const characterImage = document.getElementById("character-image");
+
+        characterName.textContent = character.name;
+        characterStatus.textContent = character.status;
+        characterSpecies.textContent = character.species;
+        characterImage.innerHTML = `<img src="${character.image}" alt="${character.name}">`;
     } else {
-      console.log("ID del personatge no vàlid");
+        console.log("ID del personatge no vàlid");
     }
-  }
-  
-  const personatgesBtn = document.getElementById("personatges-btn");
-  personatgesBtn.addEventListener("click", handlePersonatgesClick);
-  
+}
+
+const personatgesBtn = document.getElementById("personatges-btn");
+personatgesBtn.addEventListener("click", handlePersonatgesClick);
+
 // Funció per gestionar el clic al botó "Localitzacions"
-function handleLocalitzacionsClick() {
+async function handleLocalitzacionsClick() {
     const locationId = prompt("Introdueix l'ID de la localització:");
-  
+
     if (locationId !== null && locationId.trim() !== "") {
-      fetch(`https://rickandmortyapi.com/api/location/${locationId}`)
-        .then(response => response.json())
-        .then(data => {
-          const residentsList = document.getElementById("residents-list");
-          const locationName = document.getElementById("location-name");
-          const locationType = document.getElementById("location-type");
-  
-          locationName.textContent = data.name;
-          locationType.textContent = data.type;
-  
-          const residents = data.residents;
-          let residentsHTML = "";
-  
-          if (residents.length > 0) {
+        const location = await getLocalitzacio(locationId);
+        const residentsList = document.getElementById("residents-list");
+        const locationName = document.getElementById("location-name");
+        const locationType = document.getElementById("location-type");
+
+        locationName.textContent = location.name;
+        locationType.textContent = location.type;
+
+        const residents = location.residents;
+        let residentsHTML = "";
+
+        if (residents.length > 0) {
             residents.forEach(resident => {
-              residentsHTML += `<li>${resident.name}</li>`;
+                residentsHTML += `<li>${resident.name}</li>`;
             });
-          } else {
+        } else {
             residentsHTML = "<li>No hi ha residents enregistrats</li>";
-          }
-  
-          residentsList.innerHTML = residentsHTML;
-        })
-        .catch(error => console.log(error));
+        }
+
+        residentsList.innerHTML = residentsHTML;
     } else {
-      console.log("ID de la localització no vàlid");
+        console.log("ID de la localització no vàlid");
     }
-  }
-  
-  const localitzacionsBtn = document.getElementById("localitzacions-btn");
-  localitzacionsBtn.addEventListener("click", handleLocalitzacionsClick);
-  
+}
+
+const localitzacionsBtn = document.getElementById("localitzacions-btn");
+localitzacionsBtn.addEventListener("click", handleLocalitzacionsClick);
+
